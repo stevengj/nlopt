da4be2374bbdb25b36459dfc193e237a4639323f
stevenj <stevenj@alum.mit.edu>
2008-08-18 18:40:30 -0400

    missing .m files
    darcs-hash:20080818224030-c8de0-946323047197a14252c1e3f54f0d63dac85bd3b6.gz

---
+++ octave/NLOPT_LD_LBFGS_NOCEDAL.m
@@ -0,0 +1,5 @@
+% NLOPT_LD_LBFGS_NOCEDAL: original NON-FREE L-BFGS code by Nocedal et al. (NOT COMPILED)
+%
+% See nlopt_minimize for more information.
+function val = NLOPT_LD_LBFGS_NOCEDAL
+  val = 11;
---
+++ octave/NLOPT_LD_MMA.m
@@ -0,0 +1,5 @@
+% NLOPT_LD_MMA: Method of Moving Asymptotes (MMA) (local, derivative)
+%
+% See nlopt_minimize for more information.
+function val = NLOPT_LD_MMA
+  val = 25;
