12cd6d78b64c216bb78dfc0a50c44bc16cf170ed
stevenj <stevenj@alum.mit.edu>
2011-03-22 16:13:00 -0400

    fixes to windows setup.py; thanks to Robert G for the patch
    darcs-hash:20110322201300-c8de0-7caeb9ab8e893d3c8ec0be13c96959be1234c8d0.gz

--- BUILD-MINGW.sh
+++ BUILD-MINGW.sh
@@ -56,12 +56,14 @@ nlopt_module = Extension('_nlopt',
                            sources=['nlopt-python.cpp'],
                            libraries=['libnlopt-0'],
                            )
+import numpy
 setup (name = 'nlopt',
        version = '${nlopt_vers}',
        author      = "Steven G. Johnson",
        description = """NLopt nonlinear-optimization library""",
        ext_modules = [nlopt_module],
        py_modules = ["nlopt"],
+       include_dirs = ['.', numpy.get_include()],
        )
 EOF
 
