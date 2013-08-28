ea3e85e12caf9083bb6638ac6076de40af9828aa
Steven G. Johnson <stevenj@alum.mit.edu>
2013-08-28 10:40:35 -0400

    fix missing README, remove darcs cruft

--- autogen.sh
+++ autogen.sh
@@ -16,6 +16,8 @@ done
 
 touch swig/nlopt.scm.in
 
+cp README.md README
+
 # paranoia: sometimes autoconf doesn't get things right the first time
 autoreconf --verbose --install --symlink --force
 autoreconf --verbose --install --symlink --force
