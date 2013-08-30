1e6436fa21a2739317f0f7fe3335eac7ac242ae2
xantares <xantares09@hotmail.com>
2013-08-30 16:17:09 +0200

    Added a no-configure option to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 configure_args=""
+configure="yes"
 
 while test $# -ge 1; do
     case $1 in
@@ -9,19 +10,30 @@ while test $# -ge 1; do
 	--disable-*) configure_args="$configure_args $1" ;;
 	--with-*) configure_args="$configure_args $1" ;;
 	--without-*) configure_args="$configure_args $1" ;;
+	--no-configure) configure="no" ;;
 	*) echo "unknown argument $1"; exit 1 ;;
     esac
     shift
 done
 
-./bootstrap.sh
+touch swig/nlopt.scm.in
 
-config=good # hackery so darcs_test still outputs config.log w/failed configure
+cp README.md README
 
-./configure --enable-maintainer-mode $configure_args || config=bad
+# paranoia: sometimes autoconf doesn't get things right the first time
+autoreconf --verbose --install --symlink --force
+autoreconf --verbose --install --symlink --force
+autoreconf --verbose --install --symlink --force
 
-if test x$verbose = xyes; then
-    cat config.log
+if test "$configure" = "yes"
+then
+  config=good # hackery so darcs_test still outputs config.log w/failed configure
+
+  ./configure --enable-maintainer-mode $configure_args || config=bad
+
+  if test x$verbose = xyes; then
+      cat config.log
+  fi
+
+  test $config = bad && exit 1
 fi
-
-test $config = bad && exit 1
