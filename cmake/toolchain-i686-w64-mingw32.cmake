27856dd61183a2611b9b26e01f68c6a3342edb1d
Amro <amroamroamro@gmail.com>
2016-07-29 15:21:11 +0300

    minor edits

--- cmake/toolchain-i686-w64-mingw32.cmake
+++ cmake/toolchain-i686-w64-mingw32.cmake
@@ -1,8 +1,18 @@
+# Define the environment for cross compiling from Linux to Win32
 set (CMAKE_SYSTEM_NAME Windows)
 
 # specify the cross compiler
 set (CMAKE_C_COMPILER i686-w64-mingw32-gcc)
 set (CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
+
+# set the resource compiler (RHBZ #652435)
+set (CMAKE_RC_COMPILER i686-w64-mingw32-windres)
+set (CMAKE_MC_COMPILER i686-w64-mingw32-windmc)
+
+# These are needed for compiling lapack (RHBZ #753906)
+set (CMAKE_Fortran_COMPILER i686-w64-mingw32-gfortran)
+set (CMAKE_AR:FILEPATH i686-w64-mingw32-ar)
+set (CMAKE_RANLIB:FILEPATH i686-w64-mingw32-ranlib)
 
 # where is the target environment
 set (CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
@@ -18,15 +28,6 @@ set (CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 set (QT_BINARY_DIR /usr/i686-w64-mingw32/bin /usr/bin)
 set (QT_INCLUDE_DIRS_NO_SYSTEM ON)
 
-# set the resource compiler (RHBZ #652435)
-set (CMAKE_RC_COMPILER i686-w64-mingw32-windres)
-set (CMAKE_MC_COMPILER i686-w64-mingw32-windmc)
-
-# override boost thread component suffix as mingw-w64-boost is compiled with threadapi=win32
+# override boost thread component suffix
+# as mingw-w64-boost is compiled with threadapi=win32
 set (Boost_THREADAPI win32)
-
-# These are needed for compiling lapack (RHBZ #753906)
-set (CMAKE_Fortran_COMPILER i686-w64-mingw32-gfortran)
-set (CMAKE_AR:FILEPATH i686-w64-mingw32-ar)
-set (CMAKE_RANLIB:FILEPATH i686-w64-mingw32-ranlib)
-
