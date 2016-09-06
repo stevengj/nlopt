da29563f28f0dbfbf7033f0f2d8aca9f6ab2db88
Amro <amroamroamro@gmail.com>
2016-09-06 23:48:15 +0300

    findNumpy updates
    - use FindPackageHandleStandardArgs to handle find_package args
    - simplify by have separate calls to query numpy version and includes
    - use find_path to check headers

--- cmake/FindNumPy.cmake
+++ cmake/FindNumPy.cmake
@@ -5,7 +5,7 @@
 # TODO: Update to provide the libraries and paths for linking npymath lib.
 #
 #  NUMPY_FOUND               - was NumPy found
-#  NUMPY_VERSION             - the version of NumPy found as a string
+#  NUMPY_VERSION_STRING      - the version of NumPy found as a string
 #  NUMPY_VERSION_MAJOR       - the major version number of NumPy
 #  NUMPY_VERSION_MINOR       - the minor version number of NumPy
 #  NUMPY_VERSION_PATCH       - the patch version number of NumPy
@@ -38,63 +38,92 @@
 #
 #============================================================================
 
-# Finding NumPy involves calling the Python interpreter
-if(NumPy_FIND_REQUIRED)
-    find_package(PythonInterp REQUIRED)
-else()
-    find_package(PythonInterp)
-endif()
+# finding NumPy involves calling the Python interpreter
+if (NOT DEFINED PYTHONINTERP_FOUND)
+  if (NumPy_FIND_REQUIRED)
+    find_package (PythonInterp REQUIRED)
+  else ()
+    find_package (PythonInterp)
+  endif ()
+endif ()
 
-if(NOT PYTHONINTERP_FOUND)
-    set(NUMPY_FOUND FALSE)
-    return()
-endif()
-
-execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c"
-    "import numpy as n; print(n.__version__); print(n.get_include());"
-    RESULT_VARIABLE _NUMPY_SEARCH_SUCCESS
-    OUTPUT_VARIABLE _NUMPY_VALUES_OUTPUT
-    ERROR_VARIABLE _NUMPY_ERROR_VALUE
+# find NumPy
+if (PYTHONINTERP_FOUND)
+  # get numpy version
+  execute_process (
+    COMMAND
+      "${PYTHON_EXECUTABLE}" "-c" "import numpy; print(numpy.__version__);"
+    RESULT_VARIABLE _NUMPY_RESULT_CODE
+    OUTPUT_VARIABLE _NUMPY_OUTPUT
+    ERROR_VARIABLE _NUMPY_OUTPUT_ERROR
     OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-if(NOT _NUMPY_SEARCH_SUCCESS MATCHES 0)
-    if(NumPy_FIND_REQUIRED)
-        message(FATAL_ERROR
-            "NumPy import failure:\n${_NUMPY_ERROR_VALUE}")
-    endif()
-    set(NUMPY_FOUND FALSE)
-    return()
-endif()
+  # check version
+  unset (NUMPY_VERSION_STRING)
+  if (_NUMPY_OUTPUT AND _NUMPY_RESULT_CODE MATCHES 0)
+    # parse version
+    string (REGEX MATCH "^([0-9]+)\\.([0-9]+)\\.([0-9]+)"
+      _MATCH "${_NUMPY_OUTPUT}")
+    if (_MATCH)
+      set (NUMPY_VERSION_MAJOR ${CMAKE_MATCH_1})
+      set (NUMPY_VERSION_MINOR ${CMAKE_MATCH_2})
+      set (NUMPY_VERSION_PATCH ${CMAKE_MATCH_3})
+      set (NUMPY_VERSION_STRING
+        "${NUMPY_VERSION_MAJOR}.${NUMPY_VERSION_MINOR}.${NUMPY_VERSION_PATCH}")
+      math (EXPR NUMPY_VERSION_DECIMAL "(${NUMPY_VERSION_MAJOR} * 10000) +
+        (${NUMPY_VERSION_MINOR} * 100) + ${NUMPY_VERSION_PATCH}")
+    endif ()
+  elseif (NumPy_FIND_REQUIRED)
+    message (SEND_ERROR "Could NOT get NumPy version: ${_NUMPY_OUTPUT_ERROR}")
+  endif ()
 
-# Convert the process output into a list
-string(REGEX REPLACE ";" "\\\\;" _NUMPY_VALUES ${_NUMPY_VALUES_OUTPUT})
-string(REGEX REPLACE "\n" ";" _NUMPY_VALUES ${_NUMPY_VALUES})
-list(GET _NUMPY_VALUES 0 NUMPY_VERSION)
-list(GET _NUMPY_VALUES 1 NUMPY_INCLUDE_DIRS)
+  # get numpy include dirs
+  execute_process (
+    COMMAND
+      "${PYTHON_EXECUTABLE}" "-c" "import numpy; print(numpy.get_include());"
+    RESULT_VARIABLE _NUMPY_RESULT_CODE
+    OUTPUT_VARIABLE _NUMPY_OUTPUT
+    ERROR_VARIABLE _NUMPY_OUTPUT_ERROR
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-string(REGEX MATCH "^[0-9]+\\.[0-9]+\\.[0-9]+" _VER_CHECK "${NUMPY_VERSION}")
-if("${_VER_CHECK}" STREQUAL "")
-    # The output from Python was unexpected. Raise an error always
-    # here, because we found NumPy, but it appears to be corrupted somehow.
-    message(FATAL_ERROR
-        "Requested version and include path from NumPy, got instead:\n${_NUMPY_VALUES_OUTPUT}\n")
-    return()
-endif()
+  # check headers
+  unset (NUMPY_INCLUDE_DIR)
+  if (_NUMPY_OUTPUT AND _NUMPY_RESULT_CODE MATCHES 0)
+    # normalize path to cmake convention with '/' as separator
+    file (TO_CMAKE_PATH "${_NUMPY_OUTPUT}" _NUMPY_OUTPUT)
+    find_path (NUMPY_INCLUDE_DIR numpy/arrayobject.h
+      HINTS
+        "${_NUMPY_OUTPUT}"
+        ${PYTHON_INCLUDE_DIRS}
+    )
+    set (NUMPY_INCLUDE_DIRS ${NUMPY_INCLUDE_DIR})
+  elseif (NumPy_FIND_REQUIRED)
+    message (SEND_ERROR "Could NOT get NumPy includes: ${_NUMPY_OUTPUT_ERROR}")
+  endif ()
 
-# Make sure all directory separators are '/'
-string(REGEX REPLACE "\\\\" "/" NUMPY_INCLUDE_DIRS ${NUMPY_INCLUDE_DIRS})
+  # cleanup
+  unset (_NUMPY_RESULT_CODE)
+  unset (_NUMPY_OUTPUT)
+  unset (_NUMPY_OUTPUT_ERROR)
+  unset (_MATCH)
+endif ()
 
-# Get the major and minor version numbers
-string(REGEX REPLACE "\\." ";" _NUMPY_VERSION_LIST ${NUMPY_VERSION})
-list(GET _NUMPY_VERSION_LIST 0 NUMPY_VERSION_MAJOR)
-list(GET _NUMPY_VERSION_LIST 1 NUMPY_VERSION_MINOR)
-list(GET _NUMPY_VERSION_LIST 2 NUMPY_VERSION_PATCH)
-string(REGEX MATCH "[0-9]*" NUMPY_VERSION_PATCH ${NUMPY_VERSION_PATCH})
-math(EXPR NUMPY_VERSION_DECIMAL
-    "(${NUMPY_VERSION_MAJOR} * 10000) + (${NUMPY_VERSION_MINOR} * 100) + ${NUMPY_VERSION_PATCH}")
+# handle REQUIRED and QUIET options
+include (FindPackageHandleStandardArgs)
+if (CMAKE_VERSION VERSION_LESS 2.8.3)
+  find_package_handle_standard_args (NumPy
+    DEFAULT_MSG
+      NUMPY_INCLUDE_DIR
+      NUMPY_VERSION_STRING
+  )
+else ()
+  find_package_handle_standard_args (NumPy
+    REQUIRED_VARS
+      NUMPY_INCLUDE_DIR
+      NUMPY_VERSION_STRING
+    VERSION_VAR
+      NUMPY_VERSION_STRING
+  )
+endif ()
 
-find_package_message(NUMPY
-    "Found NumPy: version \"${NUMPY_VERSION}\" ${NUMPY_INCLUDE_DIRS}"
-    "${NUMPY_INCLUDE_DIRS}${NUMPY_VERSION}")
-
-set(NUMPY_FOUND TRUE)
+mark_as_advanced (NUMPY_INCLUDE_DIR)
