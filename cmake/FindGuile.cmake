23f085c4451843b7e7560bd9c2e83b823f50d905
Amro <amroamroamro@gmail.com>
2016-09-06 23:39:23 +0300

    findGuile: query version from config executable
    instead of parsing header file

--- cmake/FindGuile.cmake
+++ cmake/FindGuile.cmake
@@ -7,6 +7,10 @@
 #  GUILE_INCLUDE_DIRS      - the GUILE include directory
 #  GUILE_LIBRARIES         - The libraries needed to use GUILE
 #  GUILE_VERSION_STRING    - Version
+#  GUILE_VERSION_MAJOR     - major version
+#  GUILE_VERSION_MINOR     - minor version
+#  GUILE_VERSION_PATCH     - patch version
+#  GUILE_EXECUTABLE        - GUILE executable
 #  GUILE_ROOT_DIR          - prefix dir
 #  GUILE_SITE_DIR          - site dir
 #  GUILE_EXTENSION_DIR     - extension dir
@@ -75,6 +79,29 @@ if (GUILE_CONFIG_EXECUTABLE)
     COMMAND ${GUILE_CONFIG_EXECUTABLE} info extensiondir
     OUTPUT_VARIABLE GUILE_EXTENSION_DIR
     OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+  # query guile version
+  execute_process (
+    COMMAND ${GUILE_CONFIG_EXECUTABLE} --version
+    OUTPUT_VARIABLE _GUILE_VERSION
+    ERROR_VARIABLE _GUILE_VERSION
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+  set (GUILE_VERSION_MAJOR 0)
+  set (GUILE_VERSION_MINOR 0)
+  set (GUILE_VERSION_PATCH 0)
+  if (_GUILE_VERSION)
+    string (REGEX MATCH ".*([0-9]+)\\.([0-9]+)\\.([0-9]+)"
+      _MATCH "${_GUILE_VERSION}")
+    set (GUILE_VERSION_MAJOR ${CMAKE_MATCH_1})
+    set (GUILE_VERSION_MINOR ${CMAKE_MATCH_2})
+    set (GUILE_VERSION_PATCH ${CMAKE_MATCH_3})
+  endif ()
+  set (GUILE_VERSION_STRING
+    "${GUILE_VERSION_MAJOR}.${GUILE_VERSION_MINOR}.${GUILE_VERSION_PATCH}")
+
+  unset (_MATCH)
+  unset (_GUILE_VERSION)
 endif ()
 
 # Look for the header file
@@ -106,31 +133,6 @@ find_library (GUILE_LIBRARY
 )
 set (GUILE_LIBRARIES ${GUILE_LIBRARY})
 
-# check guile's version if we're using cmake >= 2.6
-if (GUILE_INCLUDE_DIR)
-  SET(GUILE_VERSION_MAJOR 0)
-  SET(GUILE_VERSION_MINOR 0)
-  SET(GUILE_VERSION_PATCH 0)
-
-  IF(NOT EXISTS "${GUILE_INCLUDE_DIR}/libguile/version.h")
-          MESSAGE(FATAL_ERROR "Found ${GUILE_INCLUDE_DIR}/libguile.h but not version.h; check your guile installation!")
-  ENDIF(NOT EXISTS "${GUILE_INCLUDE_DIR}/libguile/version.h")
-
-  # Extract the libguile version from the 'version.h' file
-  SET(GUILE_MAJOR_VERSION 0)
-  FILE(READ "${GUILE_INCLUDE_DIR}/libguile/version.h" _GUILE_VERSION_H_CONTENTS)
-
-  STRING(REGEX MATCH "#define SCM_MAJOR_VERSION[	 ]+([0-9])" _MATCH "${_GUILE_VERSION_H_CONTENTS}")
-  SET(GUILE_VERSION_MAJOR ${CMAKE_MATCH_1})
-  STRING(REGEX MATCH "#define SCM_MINOR_VERSION[	 ]+([0-9]+)" _MATCH "${_GUILE_VERSION_H_CONTENTS}")
-  SET(GUILE_VERSION_MINOR ${CMAKE_MATCH_1})
-  STRING(REGEX MATCH "#define SCM_MICRO_VERSION[	 ]+([0-9]+)" _MATCH "${_GUILE_VERSION_H_CONTENTS}")
-  SET(GUILE_VERSION_PATCH ${CMAKE_MATCH_1})
-
-  SET(GUILE_VERSION_STRING "${GUILE_VERSION_MAJOR}.${GUILE_VERSION_MINOR}.${GUILE_VERSION_PATCH}")
-
-endif ()
-
 #if (GUILE_FOUND AND GUILE_VERSION_MAJOR EQUAL 2)
 #  add_definitions (-DHAVE_GUILE2)
 #endif ()
@@ -140,6 +142,7 @@ include (FindPackageHandleStandardArgs)
 if (CMAKE_VERSION VERSION_LESS 2.8.3)
   find_package_handle_standard_args (Guile
     DEFAULT_MSG
+      GUILE_EXECUTABLE
       GUILE_ROOT_DIR
       GUILE_INCLUDE_DIRS
       GUILE_LIBRARIES
@@ -147,14 +150,18 @@ if (CMAKE_VERSION VERSION_LESS 2.8.3)
 else ()
   find_package_handle_standard_args (Guile
     REQUIRED_VARS
+      GUILE_EXECUTABLE
       GUILE_ROOT_DIR
       GUILE_INCLUDE_DIRS
       GUILE_LIBRARIES
+      GUILE_VERSION_STRING
     VERSION_VAR
       GUILE_VERSION_STRING)
 endif ()
 
 mark_as_advanced (
+  GUILE_CONFIG_EXECUTABLE
+  GUILE_EXECUTABLE
   GUILE_INCLUDE_DIR
   GUILE_LIBRARY
 )
