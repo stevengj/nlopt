e6d60a07d2c1f7f288934d0496942ee808e2a5b8
Julien Schueller <schueller@phimeca.com>
2023-07-12 16:23:31 +0200

    CMake: Cleanup generate-cpp.cmake

--- cmake/generate-cpp.cmake
+++ cmake/generate-cpp.cmake
@@ -1,5 +1,3 @@
-cmake_minimum_required (VERSION 3.2)
-
 # generate nlopt.hpp from nlopt-in.hpp
 file (WRITE ${CMAKE_CURRENT_BINARY_DIR}/nlopt.hpp "")
 file (STRINGS ${API_SOURCE_DIR}/nlopt-in.hpp NLOPT_HPP_LINES)
