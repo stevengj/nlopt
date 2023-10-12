4e07a1c6ba07afc9d1ffc69fb5bb47fbaf9dc254
Steven G. Johnson <stevenj@alum.mit.edu>
2023-10-12 10:21:18 -0400

    add NLOPT_GN_AGS constant for Octave/Matlab

---
+++ src/octave/NLOPT_GN_AGS.m
@@ -0,0 +1,6 @@
+% NLOPT_GN_AGS: global derivative-free optimization
+% with nonlinear inequality constraints.
+%
+% See nlopt_minimize for more information.
+function val = NLOPT_GN_AGS
+  val = 43;
